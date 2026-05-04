46f0183ac16fd312dd236132575ef5237c2af52d
nastya564 <nastya56499@yandex.ru>
2026-05-04 07:36:25 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
@@ -8,4 +10,8 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
 })
